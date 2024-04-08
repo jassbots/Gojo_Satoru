f3c07547232167d882fe05f1f1a8733d9fcbdc17
JASS BOTS <157136102+jassbots@users.noreply.github.com>
2024-04-08 21:04:26 +0530

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -111,7 +111,7 @@ Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Gojo �
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](https://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join my [News Channel](https://t.me/entity_unavailable) to get information on all the latest updates."""
 
             await m.reply_photo(
                 photo=str(choice(StartPic)),
@@ -241,7 +241,7 @@ async def help_menu(_, m: Message):
             ou = await gen_cmds_kb(m)
             keyboard = ikb(ou, True)
             msg = f"""
-Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Gojo✨.
+Hey **[{m.from_user.first_name}](http://t.me/{m.from_user.username})**!I am Yo Helpbot✨.
 I'm here to help you manage your group(s)!
 Commands available:
 × /start: Start the bot
